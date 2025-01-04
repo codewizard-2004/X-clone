94d1310f17fc81006df2cec0325ef4c0af05881a
codewizard-2004 <amalv2004@gmail.com>
2025-01-04 18:35:14 +0530

    backend complete

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import postRoutes from "./routes/post.routes.js"
+import notificationRoutes from "./routes/notification.routes.js"
 
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
@@ -29,6 +30,7 @@ app.use(cookieParser());
 app.use("/api/auth" , authRoutes);
 app.use("/api/user" , userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/notifications" , notificationRoutes)
 
 app.listen(PORT , ()=>{
     console.log(`Server is running at port ${PORT}`);
