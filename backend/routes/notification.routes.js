ae49a36ff4cad7697eaf4dfce5899ab2160b653c
codewizard-2004 <amalv2004@gmail.com>
2025-02-28 09:49:19 +0530

    notifitcation error fixed

--- backend/routes/notification.routes.js
+++ backend/routes/notification.routes.js
@@ -1,16 +1,10 @@
 import express from "express";
-import { 
-    getNotifications,
-    deleteNotifications,
-    deleteNotification
- } from "../controllers/notification.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
+import { deleteNotifications, getNotifications } from "../controllers/notification.controller.js";
 
 const router = express.Router();
 
-router.get("/all", protectRoute, getNotifications);
-router.delete("/" , protectRoute , deleteNotifications);
-router.delete("/:id" , protectRoute , deleteNotification);
+router.get("/", protectRoute, getNotifications);
+router.delete("/", protectRoute, deleteNotifications);
 
-
-export default router
+export default router;
--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,36 +1,54 @@
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
-
+import { toast } from "react-hot-toast";
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
+import {useQuery, useMutation, useQueryClient} from "@tanstack/react-query"
 
 const NotificationPage = () => {
-	const isLoading = false;
-	const notifications = [
-		{
-			_id: "1",
-			from: {
-				_id: "1",
-				username: "johndoe",
-				profileImg: "/avatars/boy2.png",
-			},
-			type: "follow",
-		},
-		{
-			_id: "2",
-			from: {
-				_id: "2",
-				username: "janedoe",
-				profileImg: "/avatars/girl1.png",
-			},
-			type: "like",
-		},
-	];
+	const queryClient = useQueryClient();
+	const {data: notifications,isLoading} = useQuery({
+		queryKey:["notifications"],
+		queryFn: async()=>{
+			try {
+				const res = await fetch("/api/notifications");
+				const data = await res.json();
+				if(!res.ok){
+					throw new Error(data.message || "An Error Occured");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error.message || "An Error Occured");
+			}
+		}
+	})
 
-	const deleteNotifications = () => {
-		alert("All notifications deleted");
-	};
+	const {mutate: deleteNotifications} = useMutation({
+		mutationFn: async()=>{
+			try {
+				const res = await fetch("/api/notifications",{
+					method:"DELETE"
+				});
+				const data = await res.json();
+				if(!res.ok){
+					throw new Error(data.message || "An Error Occured");
+				}
+				return data;
+			} catch (error) {
+				throw Error(error.message || "An Error Occured");
+			}
+		},
+		onSuccess: ()=>{
+			toast.success("Notifications deleted successfully");
+			queryClient.invalidateQueries({queryKey:["notifications"]});
+		},
+		onError: (error)=>{
+			toast.error(error.message);
+		}
+	})
+
+	
 
 	return (
 		<>
